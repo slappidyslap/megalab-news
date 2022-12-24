46ab0aa9a11e3655da012d1b5e39ce948e5306eb
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2022-12-24 22:45:52 +0600

    a d hashcode correctness test for the post

--- src/test/java/kg/musabaev/megalabnews/PostEntityTest.java
+++ src/test/java/kg/musabaev/megalabnews/PostEntityTest.java
@@ -1,6 +1,8 @@
 package kg.musabaev.megalabnews;
 
 import kg.musabaev.megalabnews.model.Post;
+import lombok.extern.log4j.Log4j2;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -8,12 +10,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@Log4j2
 public class PostEntityTest {
 
     @Autowired
@@ -26,5 +32,23 @@ public class PostEntityTest {
         transientPost.setContent("it is content");
         Post persistedPost = entityManager.persistAndFlush(transientPost);
         assertThat(LocalDate.now(), is(persistedPost.getCreatedDate()));
+    }
+
+    @Test
+    void shouldBeSameHashcode() {
+        Set<Post> posts = new HashSet<>();
+
+        Post transientPost = new Post();
+        transientPost.setTitle("title");
+        transientPost.setCreatedDate(LocalDate.now());
+
+        posts.add(transientPost);
+
+        Post persistedPost = entityManager.persistAndFlush(transientPost);
+
+        assertThat(persistedPost, in(posts));
+        assertThat(persistedPost.hashCode(), is(transientPost.hashCode()));
+        log.info("persisted post's hashcode: {}", persistedPost.hashCode());
+        log.info("transient post's hashcode: {}", transientPost.hashCode());
     }
 }
