f72678f8f2538f2eab2ba24b5632ac522f3cfd5c
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-01-15 12:52:51 +0600

    add exception handler

--- src/main/java/kg/musabaev/megalabnews/exception/RestApiExceptionHandler.java
+++ src/main/java/kg/musabaev/megalabnews/exception/RestApiExceptionHandler.java
@@ -2,14 +2,24 @@ package kg.musabaev.megalabnews.exception;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
-public class RestApiExceptionHandler {
+public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(MaxUploadSizeExceededException.class)
 	void handleMaxUploadSizeExceededException(HttpServletResponse response) {
@@ -49,7 +59,23 @@ public class RestApiExceptionHandler {
 	@ExceptionHandler({
 			UserNotFoundException.class, PostNotFoundException.class,
 			CommentNotFoundException.class, ResponseStatusNotFoundException.class})
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	void setNotFoundStatusCode() {
+	@ResponseStatus(NOT_FOUND)
+	Map<String, Boolean> setNotFoundStatusCode(Exception e) {
+		return Map.of(
+				"isPostNotFound", e.getClass() == PostNotFoundException.class,
+				"isCommentNotFound", e.getClass() == CommentNotFoundException.class,
+				"isUserNotFound", e.getClass() == UserNotFoundException.class,
+				"isAnotherResourceNotFound", e.getClass() == ResponseStatusNotFoundException.class
+		);
+	}
+
+	@Override
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex,
+			HttpHeaders headers,
+			HttpStatusCode status,
+			WebRequest request) {
+		return super.handleMethodArgumentNotValid(ex, headers, status, request);
 	}
 }
